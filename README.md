e06902d3480f14a615208db1774e9fdae7d012eb
Nik <nikitama1hr@gmail.com>
2023-05-12 12:19:24 +0300

    update customSecretKey.js secret-data (bad)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Documentation 
 
 * [API DOCS](https://documenter.getpostman.com/view/13759288/2s93ebUXEG)
-* [DEMO VIEW](link-not-ready)
+* [DEMO VIEW](https://w-test-1.onrender.com/)
 
 --- 
 #### I have some ideas on how to improve the code
--- customSecretKey.js
+++ customSecretKey.js
@@ -6,11 +6,12 @@ const customSecretKey = {
         PORT: 5000
     },
     db: {
-        host: 'localhost',
+        // I know it's very very bad!!!
+        host: 'dpg-chevnporddl9bukk7qb0-a.oregon-postgres.render.com',
         port: 5432,
-        database: 'postgres',
-        user: 'postgres',
-        password: '1234',
+        database: 'w_test',
+        user: 'w_test_user',
+        password: 'xnB3nJHQgWW5ZYIsbn6b3NpVsJJWsqso',
     },
     jwt: {
         TOKEN_KEY: 'exampleSecret'
